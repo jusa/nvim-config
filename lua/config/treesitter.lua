a55b6d5b4e1c8cbeb56859d3e59824e48c4f68f3
Enni Hämäläinen <enni@hilvi.org>
2026-04-07 11:27:31 +0300

    treesitter: Update for nvim 0.12.x
    Apparently nvim-treesitter was rewritten for nvim 0.12 and for one the
    way it's configured is changed as well.

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,4 @@
-local configs = require("nvim-treesitter.configs")
-configs.setup {
-  -- Add a language of your choice
-  ensure_installed = {
+local languages = {
     "c",
     "cpp",
     "java",
@@ -11,22 +8,18 @@ configs.setup {
     "markdown",
     "python",
     "vimdoc",
-  },
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  }
 }
+
+local treesitter = require('nvim-treesitter')
+treesitter.setup()
+treesitter.install(languages)
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = languages,
+    callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,5 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ':TSUpdate',
 }
